0392cb2aeb594630a818327a3493b669bc74a6c0
Matheus Antonio <matheusantonio232@gmail.com>
2022-01-29 14:50:13 -0300

    chore: adicionando primevue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.use(router).mount('#app');
